75cb02d877183469f8b3294365509cdd92f00e07
PlayBoi Baby <ferdinandcharles6@gmail.com>
2023-11-03 09:12:36 +0100

    personal data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -37,7 +37,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: list[str]):
+    def __init__(self, fields: List[str]):
         """
         Initialize class
         """
